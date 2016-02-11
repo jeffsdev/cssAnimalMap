65443f72fb039c334514953b150fc44cab9c64dc
Jeff Seymour and lindsey raffa <lindsey.raffa@gmail.com>
2016-02-10 16:05:10 -0800

    add pictures add style add function font changes

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  $('.panda').mouseenter(function() {
+    $('.asia').show();
+  })
+  $('.panda').mouseleave(function() {
+    $('.asia').hide();
+  })
+
+
+  $('.wombat').mouseenter(function() {
+    $('.australia').show();
+  })
+  $('.wombat').mouseleave(function() {
+    $('.australia').hide();
+  })
+
+
+  $('.whale').mouseenter(function() {
+    $('.antarctica').show();
+  })
+  $('.whale').mouseleave(function() {
+    $('.antarctica').hide();
+  })
+
+
+  $('.rhino').mouseenter(function() {
+    $('.africa').show();
+  })
+  $('.rhino').mouseleave(function() {
+    $('.africa').hide();
+  })
+
+
+  $('.bison').mouseenter(function() {
+    $('.namerica').show();
+  })
+  $('.bison').mouseleave(function() {
+    $('.namerica').hide();
+  })
+
+
+  $('.seal').mouseenter(function() {
+    $('.europe').show();
+  })
+  $('.seal').mouseleave(function() {
+    $('.europe').hide();
+  })
+
+
+  $('.sandcat').mouseenter(function() {
+    $('.middleeast').show();
+  });
+  $('.sandcat').mouseleave(function() {
+    $('.middleeast').hide();
+  });
+
+
+  $('.littlecat').mouseenter(function() {
+    $('.samerica').show();
+  })
+  $('.littlecat').mouseleave(function() {
+    $('.samerica').hide();
+  })
+
+
+  $('.spidermonkey').mouseenter(function() {
+    $('.camerica').show();
+  })
+  $('.spidermonkey').mouseleave(function() {
+    $('.camerica').hide();
+  })
+
+
+  $('.saiga').mouseenter(function() {
+    $('.siberia').show();
+  })
+  $('.saiga').mouseleave(function() {
+    $('.siberia').hide();
+  })
+
+});
